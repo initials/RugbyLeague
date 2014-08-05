96ff28cafa63f7af3b7169b6b46e154ef9936040
Shane Brouwer <shane@findesign.com.au>
2014-08-05 16:41:16 +1000

    x

---
+++ RugbyLeague/RugbyLeague/RugbyLeague/GameSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace RugbyLeague
+{
+    public class GameSettings
+    {
+        #region Fields
+
+        public string Name;
+        public float DefaultAmount;
+        public float MinAmount;
+        public float MaxAmount;
+        public float Increment;
+        public float GameValue;
+
+        #endregion
+
+        public GameSettings(string name,
+                            float gameValue,
+                            float defaultAmount,
+                            float minAmount,
+                            float maxAmount,
+                            float increment)
+        {
+            Name = name;
+            DefaultAmount = defaultAmount;
+            MinAmount = minAmount;
+            MaxAmount = maxAmount;
+            Increment = increment;
+            GameValue = gameValue;
+
+        }
+    }
+}
